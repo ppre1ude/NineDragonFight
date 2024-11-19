401b16ce44ab2a029cdbfbb12f49ab41b4f6fd66
최대한 <daehoney9889@gmail.com>
2024-11-19 16:01:33 +0900

    클라이언트 서버형 수정

--- Client.py
+++ Client.py
@@ -1,43 +1,94 @@
 import socket
 import pickle
+from prettytable import PrettyTable
+from colorama import init, Fore, Style
+
+init()
+
 
 class GameClient:
     def __init__(self):
-        self.host = input("Enter the server IP address: ")  # 서버 IP 주소 입력
-        self.port = int(input("Enter the server port: "))   # 서버 포트 입력
+        self.host = input("Enter the server IP address: ").strip()  # 서버 IP 주소 입력
+        self.port = int(input("Enter the server port: ").strip())  # 서버 포트 입력
+
+    def clear_screen(self):
+        """화면을 정리."""
+        import os
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def display_board(self, player1_name, player2_name, player1_points, player2_points, player1_tiles, player2_tiles, round_number):
+        """보드판을 출력."""
+        table = PrettyTable()
+        table.field_names = ["Player", "Score", "Remaining Tiles"]
+
+        table.add_row([player1_name, player1_points, ", ".join(map(str, player1_tiles))])
+        table.add_row([player2_name, player2_points, ", ".join(map(str, player2_tiles))])
+
+        self.clear_screen()
+        print("\n" + "=" * 60)
+        print(f"             ⚔  ROUND {round_number} ⚔")
+        print(Style.BRIGHT + Fore.YELLOW + "       Game Board" + Style.RESET_ALL)
+        print("=" * 60)
+        print(table)
+        print("=" * 60)
 
     def start(self):
+        """클라이언트 메인 실행 함수."""
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-            client_socket.connect((self.host, self.port))
-            print("Connected to the game server!")
+            try:
+                # 서버에 연결
+                client_socket.connect((self.host, self.port))
+                print(Fore.GREEN + "Connected to the game server!" + Style.RESET_ALL)
 
-            while True:
-                # 서버로부터 메시지 수신
-                data = pickle.loads(client_socket.recv(1024))
-                if data["type"] == "request_tile":
-                    # 타일 선택 요청
-                    print(data["message"])
-                    print(f"Your tiles: {data['tiles']}")
-                    tile = int(input("Enter your tile: "))
-                    client_socket.send(pickle.dumps({"tile": tile}))
+                while True:
+                    # 서버로부터 메시지 수신
+                    data = pickle.loads(client_socket.recv(1024))
 
-                elif data["type"] == "round_result":
-                    # 라운드 결과 수신
-                    print(f"Round {data['round']} results:")
-                    print(f"Player 0 chose: {data['chosen_tiles'][0]}")
-                    print(f"Player 1 chose: {data['chosen_tiles'][1]}")
-                    winner = data["winner"]
-                    if winner is not None:
-                        print(f"Winner: Player {winner}")
-                    else:
-                        print("Draw!")
-                    print(f"Scores: Player 0 - {data['scores'][0]}, Player 1 - {data['scores'][1]}")
+                    if data["type"] == "request_tile":
+                        # 보드판 출력
+                        self.display_board(
+                            player1_name=data["player1_name"],
+                            player2_name=data["player2_name"],
+                            player1_points=data["player1_points"],
+                            player2_points=data["player2_points"],
+                            player1_tiles=data["player1_tiles"],
+                            player2_tiles=data["player2_tiles"],
+                            round_number=data["round_number"]
+                        )
 
-                elif data["type"] == "game_over":
-                    # 게임 종료 메시지 수신
-                    print("Game over!")
-                    print(f"Final scores: Player 0 - {data['scores'][0]}, Player 1 - {data['scores'][1]}")
-                    break
+                        # 타일 선택 요청
+                        print(Fore.BLUE + f"{data['message']}" + Style.RESET_ALL)
+                        tile = int(input("Enter your tile: "))
+                        client_socket.send(pickle.dumps({"tile": tile}))
+
+                    elif data["type"] == "round_result":
+                        # 라운드 결과 출력
+                        print(f"\nRound {data['round']} results:")
+                        print(f"{data['player1_name']} chose: {data['player1_tile']}")
+                        print(f"{data['player2_name']} chose: {data['player2_tile']}")
+                        winner = data["winner"]
+                        if winner == "Draw":
+                            print(Fore.YELLOW + "It's a draw!" + Style.RESET_ALL)
+                        else:
+                            print(Fore.BLUE + f"Winner: {winner}" + Style.RESET_ALL)
+
+                    elif data["type"] == "game_over":
+                        # 게임 종료 메시지 출력
+                        self.display_board(
+                            player1_name=data["player1_name"],
+                            player2_name=data["player2_name"],
+                            player1_points=data["player1_points"],
+                            player2_points=data["player2_points"],
+                            round_number=data["round_number"]
+                        )
+                        print("\n" + "=" * 60)
+                        print(Style.BRIGHT + Fore.GREEN + "                GAME OVER!" + Style.RESET_ALL)
+                        print(f"Final scores: {data['player1_name']} - {data['player1_points']}, {data['player2_name']} - {data['player2_points']}")
+                        print("=" * 60)
+                        break
+
+            except Exception as e:
+                print(Fore.RED + f"An error occurred: {e}" + Style.RESET_ALL)
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 import socket
 import threading
 import pickle
+from prettytable import PrettyTable
+from colorama import init, Fore, Style
 from play_game import determine_winner
+
+init()
+
 
 class GameServer:
     def __init__(self, host="127.0.0.1", port=12345):
@@ -12,65 +17,99 @@ class GameServer:
         self.tiles = {0: [i for i in range(1, 10)], 1: [i for i in range(1, 10)]}  # 각 플레이어의 타일
         self.round = 1
         self.max_rounds = 9
+        self.is_self_player = False  # 서버가 클라이언트로 참여하는지 여부
 
-    def handle_client(self, client, player_id):
+    def clear_screen(self):
+        """화면을 정리."""
+        import os
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    def display_board(self, player1_name, player2_name):
+        """보드판을 출력."""
+        table = PrettyTable()
+        table.field_names = ["Player", "Score", "Remaining Tiles"]
+
+        table.add_row([player1_name, self.scores[0], ", ".join(map(str, self.tiles[0]))])
+        table.add_row([player2_name, self.scores[1], ", ".join(map(str, self.tiles[1]))])
+
+        self.clear_screen()
+        print("\n" + "=" * 60)
+        print(f"             ⚔  ROUND {self.round} ⚔")
+        print(Style.BRIGHT + Fore.YELLOW + "       Game Board" + Style.RESET_ALL)
+        print("=" * 60)
+        print(table)
+        print("=" * 60)
+
+    def handle_client(self, client, player_id, player_names):
         try:
             while self.round <= self.max_rounds:
-                # 타일 요청
-                client.send(pickle.dumps({
-                    "type": "request_tile",
-                    "message": f"Choose a tile for Round {self.round}",
-                    "tiles": self.tiles[player_id]
-                }))
+                # 보드판 출력
+                self.display_board(player_names[0], player_names[1])
 
-                # 클라이언트로부터 선택된 타일 받기
-                data = pickle.loads(client.recv(1024))
-                chosen_tile = data["tile"]
-                self.tiles[player_id].remove(chosen_tile)
-
-                # 결과 계산
-                if player_id == 0:
-                    self.client_0_tile = chosen_tile
+                # 타일 요청 메시지
+                if client == "self":  # 서버 자신이 참여하는 경우
+                    print(f"{player_names[player_id]}'s turn")
+                    print(f"Your tiles: {self.tiles[player_id]}")
+                    tile = int(input("Enter your tile: "))
+                    self.tiles[player_id].remove(tile)
+                    setattr(self, f"client_{player_id}_tile", tile)
                 else:
-                    self.client_1_tile = chosen_tile
+                    client.send(pickle.dumps({
+                        "type": "request_tile",
+                        "message": f"{player_names[player_id]}'s turn! Choose a tile for Round {self.round}",
+                        "round_number": self.round,
+                        "player1_name": player_names[0],
+                        "player2_name": player_names[1],
+                        "player1_points": self.scores[0],
+                        "player2_points": self.scores[1],
+                        "player1_tiles": self.tiles[0],
+                        "player2_tiles": self.tiles[1]
+                    }))
 
+                    # 클라이언트로부터 선택된 타일 받기
+                    data = pickle.loads(client.recv(1024))
+                    chosen_tile = data["tile"]
+                    self.tiles[player_id].remove(chosen_tile)
+                    setattr(self, f"client_{player_id}_tile", chosen_tile)
+
+                # 두 플레이어의 타일이 모두 선택되었을 때 결과 계산
                 if hasattr(self, "client_0_tile") and hasattr(self, "client_1_tile"):
-                    # 두 플레이어의 타일이 모두 선택되었을 때 결과 계산
                     winner_tile = determine_winner(self.client_0_tile, self.client_1_tile)
                     if winner_tile == self.client_0_tile:
                         self.scores[0] += 1
-                        winner = 0
+                        winner = player_names[0]
                     elif winner_tile == self.client_1_tile:
                         self.scores[1] += 1
-                        winner = 1
+                        winner = player_names[1]
                     else:
-                        winner = None
+                        winner = "Draw"
 
-                    # 결과 전송
-                    for i, c in enumerate(self.clients):
-                        c.send(pickle.dumps({
-                            "type": "round_result",
-                            "round": self.round,
-                            "chosen_tiles": [self.client_0_tile, self.client_1_tile],
-                            "winner": winner,
-                            "scores": self.scores
-                        }))
+                    # 라운드 결과 출력
+                    print(f"\nRound {self.round} results:")
+                    print(f"{player_names[0]} chose: {self.client_0_tile}")
+                    print(f"{player_names[1]} chose: {self.client_1_tile}")
+                    if winner == "Draw":
+                        print(Fore.YELLOW + "It's a draw!" + Style.RESET_ALL)
+                    else:
+                        print(Fore.BLUE + f"Winner: {winner}" + Style.RESET_ALL)
 
                     # 라운드 진행
                     delattr(self, "client_0_tile")
                     delattr(self, "client_1_tile")
                     self.round += 1
 
-            # 게임 종료
-            for c in self.clients:
-                c.send(pickle.dumps({
-                    "type": "game_over",
-                    "scores": self.scores
-                }))
+            # 게임 종료 메시지 출력
+            self.display_board(player_names[0], player_names[1])
+            print("\n" + "=" * 60)
+            print(Style.BRIGHT + Fore.GREEN + "                GAME OVER!" + Style.RESET_ALL)
+            print(f"Final scores: {player_names[0]} - {self.scores[0]}, {player_names[1]} - {self.scores[1]}")
+            print("=" * 60)
+
         except Exception as e:
             print(f"Error: {e}")
         finally:
-            client.close()
+            if client != "self":
+                client.close()
 
     def start(self):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
@@ -78,13 +117,28 @@ class GameServer:
             server_socket.listen(2)
             print("Game Server started. Waiting for players...")
 
-            while len(self.clients) < 2:
+            # 첫 번째 플레이어 연결
+            print(f"Waiting for player 0 to connect...")
+            client, addr = server_socket.accept()
+            print(f"Player 0 connected from {addr}")
+            self.clients.append(client)
+            player_names = ["Player 0", "Player 1"]
+
+            # 서버 참여 여부 확인
+            participate = input("Do you want to participate as Player 1? (yes/no): ").strip().lower()
+            if participate == "yes":
+                print("You are now Player 1!")
+                self.is_self_player = True
+                self.clients.append("self")  # 서버를 클라이언트로 등록
+            else:
+                print(f"Waiting for player 1 to connect...")
                 client, addr = server_socket.accept()
-                print(f"Player {len(self.clients)} connected from {addr}")
+                print(f"Player 1 connected from {addr}")
                 self.clients.append(client)
 
+            # 각 플레이어의 스레드 시작
             for i, client in enumerate(self.clients):
-                threading.Thread(target=self.handle_client, args=(client, i)).start()
+                threading.Thread(target=self.handle_client, args=(client, i, player_names)).start()
 
 
 if __name__ == "__main__":
